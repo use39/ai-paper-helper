79e5fcb1954dac4f1f76224f6a58f08150ad829f
use39 <2040066109@qq.com>
2026-05-25 21:54:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from huggingface_hub import InferenceClient
 st.title("AI文献整理助手")
 
 # 这里填你的 Hugging Face token
-HF_TOKEN = "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"
+HF_TOKEN = "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"
 
 client = InferenceClient(
     provider="hf-inference",
